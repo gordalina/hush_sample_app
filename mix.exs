d2827bf0364a8ae47532fe3ff9c123c9f6ceded0
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-12-16 20:23:15 -0800

    Use master instead of specific commit

--- mix.exs
+++ mix.exs
@@ -31,9 +31,9 @@ defmodule HushSampleApp.MixProject do
       {:hush_aws_secrets_manager, ">= 0.0.0"},
       {:hush_gcp_secret_manager, ">= 0.0.0"},
       # keep this until hackney 1.16.1
-      {:hackney, git: "https://github.com/benoitc/hackney.git", tag: "a5be812", override: true},
+      {:hackney, git: "https://github.com/benoitc/hackney.git", tag: "master", override: true},
       {:jose,
-       git: "https://github.com/potatosalad/erlang-jose.git", tag: "38c95f0", override: true}
+       git: "https://github.com/potatosalad/erlang-jose.git", tag: "master", override: true}
     ]
   end
 end
